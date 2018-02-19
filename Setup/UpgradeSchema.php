4a03764c77992ff873a84ff7f7c00665c58ac7a2
Daniel van der Linden <daniel@spaggel.nl>
2018-02-19 19:25:55 +0100

    Allow long content in tooltip instead of 255 bytes

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -2,22 +2,24 @@
 
 namespace Spaggel\Tooltip\Setup;
 
+use \Magento\Framework\DB\Ddl\Table;
+
 class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
 {
     public function upgrade(\Magento\Framework\Setup\SchemaSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
     {
         $setup->startSetup();
 
-        if (version_compare($context->getVersion(), '1.1.0', '<=')) {
+        if (version_compare($context->getVersion(), '0.1.0', '<=')) {
 
             $setup->getConnection()->addColumn(
                 $setup->getTable('eav_attribute'),
                 'tooltip',
                 [
-                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    'length' => 255,
+                    'type' => Table::TYPE_TEXT,
+                    'length' => Table::MAX_TEXT_SIZE,
                     'nullable' => true,
-                    'comment' => 'A simple tooltip'
+                    'comment' => 'Attribute tooltip'
                 ]
             );
         }
