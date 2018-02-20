094640442f839b66c8d861f3eb2425cf66e519b8
daniel <daniel@tickles.nl>
2018-02-20 15:32:11 +0100

    Override core model for tooltip, add plugin to fix hardcoded template path by Magento

--- Plugin/ProductSwatchPlugin.php
+++ Plugin/ProductSwatchPlugin.php
@@ -4,8 +4,14 @@ namespace Spaggel\Tooltip\Plugin;
 
 class ProductSwatchPlugin
 {
+    /**
+     * Override setTemplate because the path to the template is hardcoded by Magento
+     * @param \Magento\Swatches\Block\Product\Renderer\Configurable $subject
+     * @param $template
+     * @return array
+     */
     public function beforeSetTemplate(
-        \Magento\ConfigurableProduct\Block\Product\View\Type\Configurable $subject,
+        \Magento\Swatches\Block\Product\Renderer\Configurable $subject,
         $template
     ) {
         return ['Spaggel_Tooltip::product/view/renderer.phtml'];
